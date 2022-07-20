0b87ca74b935543ac825336bdc423010eaacdd57
Nidhi <nidhipatel439@gmail.com>
2022-07-20 16:26:19 -0400

    refactor code

--- php-cms/admin/projects.php
+++ php-cms/admin/projects.php
@@ -56,7 +56,7 @@ $result = mysqli_query($connect, $query);
       <td align="center"><a href="projects_photo.php?id=<?php echo $record['id']; ?>">Photo</i></a></td>
       <td align="center"><a href="projects_edit.php?id=<?php echo $record['id']; ?>">Edit</i></a></td>
       <td align="center">
-        <a href="projects.php?delete=<?php echo $record['id']; ?>" onclick="javascript:confirm('Are you sure you want to delete this project?');">Delete</i></a>
+        <a href="projects.php?delete=<?php echo $record['id']; ?>" onclick="return confirm('Are you sure you want to delete this project?');">Delete</i></a>
       </td>
     </tr>
   <?php endwhile; ?>
